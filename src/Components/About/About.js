94dcffce0118432b8ba2b404c690c2348eda1ce3
mattyllier <bloodrootfox@gmail.com>
2023-05-25 10:49:21 -0500

    portfolio

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -5,7 +5,7 @@ export default function About(){
     return (
         <>
         <div>
-        <p>Welcome!</p>
+        <p>Welcome! Production Branch</p>
         <p>I'm a JavaScript and React.js developer who is has had a lifelong niche with data, algorithms and numbers.</p>
         <p>My attention to detail and passion for decrypting patterns
             and logic,  applied in my hobby of learning languages, decoding ciphers and even keeping track of license 
