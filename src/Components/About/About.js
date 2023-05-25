196bd70f39844cf6ab3764877ad5780a438fbb03
mattyllier <bloodrootfox@gmail.com>
2023-05-25 10:46:58 -0500

    portfolio

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -8,7 +8,7 @@ export default function About(){
         <p>Welcome!</p>
         <p>I'm a JavaScript and React.js developer who is has had a lifelong niche with data, algorithms and numbers.</p>
         <p>My attention to detail and passion for decrypting patterns
-            and logic, applied in my hobby of learning languages, decoding ciphers and even keeping track of license 
+            and logic,  applied in my hobby of learning languages, decoding ciphers and even keeping track of license 
             plate serial patterns</p>
         </div>
         <img src={vectImg} className='vectImg'/>
