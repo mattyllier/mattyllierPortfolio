09cc78ed6d76fb43c74680e599b9902b94785ce9
mattyllier <bloodrootfox@gmail.com>
2023-05-25 13:50:38 -0500

    portfolio

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -1,5 +1,7 @@
+import {useState} from 'react'
 import {Link} from 'react-router-dom'
 import {BiLinkExternal} from 'react-icons/bi'
+import {FaBrain} from 'react-icons/fa'
 import {CiCalculator2} from 'react-icons/ci'
 //import EncryptorIcon from './encryptorIcon.PNG'
 import EncryptorImg from './encryptorImg1.PNG'
@@ -7,20 +9,26 @@ import CalculatorImg from './calculatorImg1.PNG'
 import './Projects.css'
 
 export default function Projects(){
+    //className = onmouseEnter ? peek : default
+    //onMouseLeave
+
+    const [visible, setVisible] = useState(false)
+
     return (
         <div>
-            <h1>Projects</h1>      
-            <div id='encryptorProject'>
-                <Link className='link' to='https://mattyllier.github.io/gameProject/'>
+            <h1 id='projectsHeader'>Projects</h1>      
+            <div className='project' id='encryptorProject'>
+                <FaBrain className='linkIcon'/>
+                <Link target='_blank' className='link' to='https://mattyllier.github.io/gameProject/'>
                     Encryptor & Solve the Phrase Game
-                    <BiLinkExternal className='linkIcon'/>
+                    <div className='enterLink'>Try It<BiLinkExternal className='linkIcon'/></div>
                     <img src={EncryptorImg} className='encryptorImg'/></Link>
             </div>
-            <CiCalculator2/>
-            <div id='calculatorProject'>
-                <Link className='link' to='https://mattyllier.github.io/calculator-project/'>
+            <div className='project' id='calculatorProject'>
+                <CiCalculator2 className='linkIcon'/>
+                <Link target='_blank' className='link' to='https://mattyllier.github.io/calculator-project/'>
                     Unlimited-sized Operations Calculator
-                    <BiLinkExternal className='linkIcon'/>
+                    <p className='enterLink'>Try It<BiLinkExternal className='linkIcon'/></p>
                     <img src={CalculatorImg} className='encryptorImg'/></Link>
             </div>
         </div>
--- src/Components/Resume/Resume.js
+++ src/Components/Resume/Resume.js
@@ -1,5 +1,12 @@
+import './Resume.css'
+import DavImg from './davImg.jpg'
+
 export default function Resume(){
     return (
-        <div>Resume</div>
+        <>
+        <h3>What Qualifies Me?</h3>
+        <div>Being a graduate of Nebula Professional Development Academy, </div>
+        <img src={DavImg} className='davImg'/>
+        </>
     )
 }
